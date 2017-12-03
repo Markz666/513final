6efd59871bbba8aa2aa930559132d216c2401793
Markz666 <mzhao757@gmail.com>
2017-12-02 19:24:22 -0500

    Update comments and add plot

--- random_forest.py
+++ random_forest.py
@@ -4,13 +4,13 @@ Created on Fri Dec 1 16:17:36 2017
 
 @author: zmx
 """
-import pandas as pd
 import numpy as np
+import pandas as pd
 import time
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 
 def data_load():
-
     # use pandas to read csv file
     training_total=pd.read_csv('C:\\Users\zmx\\Desktop\\CS 513\\project\\train.csv')
     training_label=pd.DataFrame(training_total['label'])
@@ -20,38 +20,41 @@ def data_load():
     # dataframe normalization
     testing_data[testing_data!=0]=1
 
-    # train_data[train_data!=0]=1
-    m, n = training_data.shape  # dataframe is too big, use for loop
+    m, n = training_data.shape  # dataframe is too big, use for-loop to normalize the training data to avoid overflow
     for i in range(m):
         for j in range(n):
             if training_data.ix[i, j] != 0:
                 training_data.ix[i, j] = 1
-
     return training_data,training_label,testing_data
 
-#use Sklearn library，classify the test set
+#def plotNum(ind, data):
+#    plt.imshow(np.reshape(np.array(data.iloc[ind,1:]), (28, 28)), cmap="gray")
+#    plt.figure()
+#    for ii in range(1,17):
+#        plt.subplot(4,4,ii)
+#        plotNum(ii)
+#    plt.show()
+# use sklearn library，classify the testing set
 def rf_classify(training_data,training_label,testing_data):
-
     rf_clf = RandomForestClassifier() # set functions and parameters
-    rf_clf.fit(training_data,training_label.values.ravel()) #train the training data
-    rf_result=rf_clf.predict(testing_data) #predict the testing data
-
+    rf_clf.fit(training_data,training_label.values.ravel()) # train the training data
+    rf_result=rf_clf.predict(testing_data) # predict the testing data
     return rf_result
 
 if __name__=='__main__':
-    start = time.clock()
-    training_data,training_label,testing_data=data_load()#load the raw data
+    start = time.clock() # get start time
+    training_data,training_label,testing_data=data_load() # load the raw data
 
-    m,n=testing_data.shape
+    m, n = testing_data.shape
     result_labels=rf_classify(training_data,training_label,testing_data)
 
-    # convert result into dataframe
+    # convert the result into dataframe
     result={}
     Image_ID=np.arange(m)+1
     result['Label']=result_labels
     result_frame=pd.DataFrame(result,index=Image_ID)
 
-    # write the result to a csv file
+    # write the result dataframe to a csv file
     result_frame.to_csv('C:\\Users\\zmx\\Desktop\\CS 513\\project\\result_rf.csv')
-    end = time.clock()
+    end = time.clock() # get end time
     print('Total time: ', (end - start) / 60) # 75 minutes
